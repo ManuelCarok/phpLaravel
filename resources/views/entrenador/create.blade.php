4dd9e954c877b9d78cd1532064483a7a74af0509
Manuel J. Caroca Gonzalez <manuelcarok@gmail.com>
2018-06-10 17:31:00 -0400

    Layouts

--- resources/views/entrenador/create.blade.php
+++ resources/views/entrenador/create.blade.php
@@ -1,19 +1,12 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Crear Entrenador</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <div class="form-group">
-            <label for="">Nombre</label>
-            <input type="text" class="form-control" name="" id="">
-        </div>
+@extends('layouts.app') 
 
-        <button type="submit" class="btn btn-primary">Guardar</button>
-    </div>
-</body>
-</html>
+@section('title', 'Crear entrenador') 
+
+@section('content')
+<div class="form-group">
+    <label for="">Nombre</label>
+    <input type="text" class="form-control" name="" id="">
+</div>
+
+<button type="submit" class="btn btn-primary">Guardar</button>
+@endsection 
